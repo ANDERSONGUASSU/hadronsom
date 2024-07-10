ca51205c9f6188855310306c2a29b213975a7367
AndersonGuassu <andersonguassu@hotmail.com>
2024-07-10 13:00:13 -0300

    deixando mais responsivo

--- frontend/src/components/ProductsServivesImg.jsx
+++ frontend/src/components/ProductsServivesImg.jsx
@@ -22,7 +22,9 @@ const ProductsServicesImg = () => {
             src={mesa}
             alt="Manutenção"
             className={`w-full max-w-xs md:max-w-sm lg:max-w-md shadow-lg border-4 border-white rounded-lg transition-transform duration-300 ${
-              isFirstImageFront ? "z-20 transform" : "transform translate-x-16 -translate-y-14 z-10"
+              isFirstImageFront
+                ? "z-20 transform"
+                : "transform md:translate-x-16 smm: translate-x-16 -translate-y-14 z-10"
             }`}
             onClick={handleImageClick}
           />
@@ -32,7 +34,7 @@ const ProductsServicesImg = () => {
             className={`w-full max-w-xs md:max-w-sm lg:max-w-md md:-translate-x-52 ssm:-translate-x-72  -translate-y-14 shadow-lg border-4 border-white rounded-lg transition-transform duration-300 ${
               isFirstImageFront
                 ? "transform z-10"
-                : "transform md:-translate-x-72  -translate-y-0 z-20"
+                : "transform md:-translate-x-72 sm: -translate-x-94  smm: translate-y-3 z-20"
             }`}
             onClick={handleImageClick}
           />
