c5bfaeb3d47b13485dc3818705b3d3431f48a1f4
AndersonGuassu <andersonguassu@hotmail.com>
2024-07-15 08:18:37 -0300

    melhorando o menu lateral

--- frontend/src/pages/ProductsServicesPage.jsx
+++ frontend/src/pages/ProductsServicesPage.jsx
@@ -1,4 +1,6 @@
+import SectionTitle from "components/SectionTitle";
 import React, { useState } from "react";
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 
 const productsServices = [
   {
@@ -32,18 +34,65 @@ const productsServices = [
 const ProductsServicesPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("all");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const filteredProducts = productsServices
     .filter((item) => item.title.toLowerCase().includes(searchTerm.toLowerCase()))
     .filter((item) => selectedCategory === "all" || item.category === selectedCategory);
 
-  return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-8">Produtos & Serviços</h1>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-      {/* Filtro por pesquisa e categoria */}
-      <div className="mb-4 flex flex-col sm:flex-row items-center justify-between">
-        <div className="flex items-center mb-2 sm:mb-0">
+  return (
+    <div className="container mx-auto py-8 flex">
+      {/* Menu lateral */}
+      <div
+        className={`fixed top-14 left-0 w-64 h-3/5 bg-base-200 shadow-lg transform ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-48"
+        } transition-transform duration-300 z-40`}
+      >
+        <div className="flex justify-between items-center p-4 border-b">
+          <h2 className="text-xl font-bold">Categorias</h2>
+          <button onClick={toggleMenu} className="text-gray-600">
+            {isMenuOpen ? <FaAngleDoubleLeft size={24} /> : <FaAngleDoubleRight size={24} />}
+          </button>
+        </div>
+        <div className="p-4">
+          <button
+            onClick={() => setSelectedCategory("all")}
+            className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+          >
+            Todas as categorias
+          </button>
+          <button
+            onClick={() => setSelectedCategory("mesa-de-som")}
+            className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+          >
+            Mesa de Som
+          </button>
+          <button
+            onClick={() => setSelectedCategory("caixas-de-som")}
+            className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+          >
+            Caixas de Som
+          </button>
+          <button
+            onClick={() => setSelectedCategory("subwoofers")}
+            className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+          >
+            Subwoofers
+          </button>
+          {/* Adicione mais opções de categorias conforme necessário */}
+        </div>
+      </div>
+
+      {/* Conteúdo principal */}
+      <div className="flex-grow container mt-14 ssm:ml-10 sm:ml-16 lg:ml-0">
+        <SectionTitle title="Produtos" subtitle="" />
+
+        {/* Filtro por pesquisa */}
+        <div className="mb-4 flex items-center">
           <input
             type="text"
             placeholder="Pesquisar produtos..."
@@ -51,45 +100,28 @@ const ProductsServicesPage = () => {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <select
-            className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
-            value={selectedCategory}
-            onChange={(e) => setSelectedCategory(e.target.value)}
-          >
-            <option value="all">Todas as categorias</option>
-            <option value="mesa-de-som">Mesa de Som</option>
-            <option value="caixas-de-som">Caixas de Som</option>
-            <option value="subwoofers">Subwoofers</option>
-            {/* Adicione mais opções de categorias conforme necessário */}
-          </select>
         </div>
-      </div>
 
-      {/* Listagem dos produtos */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {filteredProducts.map((product) => (
-          <div key={product.id} className="bg-white shadow-md rounded-lg overflow-hidden">
-            {/* Carousel de imagens */}
-            <div className="relative h-64">
-              <img
-                src={product.images[0]}
-                alt={product.title}
-                className="w-full h-full object-cover object-center"
-              />
-              <div className="absolute bottom-0 right-0 m-4">
-                {/* Botão para abrir o carousel */}
-                <button className="text-white bg-gray-900 px-2 py-1 rounded-md hover:bg-gray-700 transition duration-300">
-                  Ver mais fotos
-                </button>
+        {/* Listagem dos produtos */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredProducts.map((product) => (
+            <div key={product.id} className="card shadow-md rounded-lg overflow-hidden">
+              {/* Carousel de imagens */}
+              <div className="h-64">
+                <img
+                  src={product.images[0]}
+                  alt={product.title}
+                  className="w-full h-full object-cover object-center"
+                />
+              </div>
+              <div className="p-4">
+                <h2 className="text-xl font-bold mb-2">{product.title}</h2>
+                <p className="text-gray-600 mb-4">{product.description}</p>
+                <p className="text-sm text-gray-500">{product.category}</p>
               </div>
             </div>
-            <div className="p-4">
-              <h2 className="text-xl font-bold mb-2">{product.title}</h2>
-              <p className="text-gray-600 mb-4">{product.description}</p>
-              <p className="text-sm text-gray-500">{product.category}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
